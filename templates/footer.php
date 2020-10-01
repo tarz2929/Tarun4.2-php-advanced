4fc412a0e91779db02b7a49f2e68d91b20404cd3
Warren Uhrich <hello@warren.codes>
2020-10-01 11:02:42 -0600

    Add footer.

---
+++ templates/footer.php
@@ -0,0 +1,10 @@
+  <footer>
+    <h2>Footer Details</h2>
+    <p>
+      &copy;
+      <?php echo date( 'Y' ); // Output current year (based on server computer time.) ?>
+      TECHCareers
+    </p>
+  </footer>
+</body>
+</html>
