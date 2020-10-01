6af8bd4e53b67eb4ecce543ad9dc92df2fe57b9c
Warren Uhrich <hello@warren.codes>
2020-10-01 10:49:31 -0600

    Initial commit; project setup; add header template.

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WebPage</title>
+
+  <!-- Style(s) -->
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+
+  <!-- Script(s) -->
+  <script type="text/JavaScript" src="js/scripts.js" defer></script>
+</head>
+<body>
+  <h1>WebPage</h1>
