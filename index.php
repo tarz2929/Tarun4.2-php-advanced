1405ce5839021d2afbb1ea61d51df3ea7bc1cee7
Warren Uhrich <hello@warren.codes>
2020-10-02 10:27:12 -0600

    Add SESSION output in index for calcHistory.

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 // GLOBAL variables are stored in PHP's
 // $GLOBALS array.
 $GLOBALS['pageTitle'] = 'Home';
@@ -12,6 +14,17 @@ include './templates/header.php';
   <?php echo $GLOBALS['pageTitle']; ?>
   page!
 </p>
+
+<?php if ( isset( $_SESSION['calcHistory'] ) ) : // Check if there IS a calc history! ?>
+  <h2>Calculator History (From Session!)</h2>
+  <ul>
+    <?php foreach ( $_SESSION['calcHistory'] as $calculation ) : ?>
+      <li>
+        <?php echo $calculation; // Output the value from our calcHistory array! ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
 
 <?php // Show our footer.
 include './templates/footer.php';
