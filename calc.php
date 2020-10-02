d632bfb17b53dd8f135684992932ee7cbbc33bd5
Warren Uhrich <hello@warren.codes>
2020-10-02 10:14:53 -0600

    Add  example and var_dump to calc.php.

--- calc.php
+++ calc.php
@@ -1,4 +1,21 @@
 <?php
+// We need to use SESSION_START to declare to PHP
+// that we want to use the $_SESSION global var!
+// It sets up a unique identifier with the browser
+// so that the array of values is associated with
+// only the one user.
+session_start();
+
+// Make sure to set a default value, if this
+// key/value pair does not yet exist in the
+// associative SESSION array!
+// *** We can't array_push() to a NULL
+//     (undefined) value!
+if ( !isset( $_SESSION['calcHistory'] ) )
+{
+  $_SESSION['calcHistory'] = array();
+}
+
 // Try to avoid use of globals unless they are absolutely necessary...
 $GLOBALS['pageTitle'] = 'PHP Calculator';
 
@@ -19,22 +36,34 @@ if ( !empty( $_GET ) ) // Check if there are any values in our array!
   switch ( $_GET['op'] )
   { // A case for each possible <option> in our form...
     case 'addition':
+      $opSymbol = '+';
       $result = $_GET['value1'] + $_GET['value2'];
       break;
     case 'subtraction':
+      $opSymbol = '-';
       $result = $_GET['value1'] - $_GET['value2'];
       break;
     case 'multiplication':
+      $opSymbol = '&times;';
       $result = $_GET['value1'] * $_GET['value2'];
       break;
     case 'division':
+      $opSymbol = '&divide;';
       $result = $_GET['value1'] / $_GET['value2'];
       break;
   }
+  // Add this result to the calculator history array!
+  array_push(
+    $_SESSION['calcHistory'],
+    "{$_GET['value1']} {$opSymbol} {$_GET['value2']} = {$result}"
+  );
 }
+echo '<pre>';
+var_dump( $_SESSION );
 // Use var_dump, much like we did in JS with console.log.
 // It outputs the data-type and value of what you pass in!
 var_dump( $result ); // What is our result, right now!?
+echo '</pre>';
 ?>
 
 <p>
