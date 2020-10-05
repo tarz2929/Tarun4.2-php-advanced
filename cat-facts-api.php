23e710fd8ead5cbfc0bdbc96aaf7a7e428fa1598
Warren Uhrich <hello@warren.codes>
2020-10-05 11:06:24 -0600

    Output Cat Fact as HTML.

--- cat-facts-api.php
+++ cat-facts-api.php
@@ -6,7 +6,17 @@ include './templates/header.php';
 // @link https://alexwohlbruck.github.io/cat-facts/docs/
 $dailyCatFactResponse = file_get_contents( 
   'https://cat-fact.herokuapp.com/facts/random'
-);
-var_dump( $dailyCatFactResponse );
+); // Testing output in var_dump...
+//var_dump( $dailyCatFactResponse );
+// If we got a response...
+if ( $dailyCatFactResponse )
+{ // Convert JSON string to PHP object.
+  $dailyCatFact = json_decode( $dailyCatFactResponse );
+  // Output daily cat fact as HTML for the browser...
+  ?>
+    <h2>Today's Cat Fact:</h2>
+    <p><?php echo $dailyCatFact->text; // Output the text property! ?></p>
+  <?php
+}
 
 include './templates/footer.php';
