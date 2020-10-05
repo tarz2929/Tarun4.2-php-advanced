5b1e35db7332061a6f80042cc0374cfaec6752ba
Warren Uhrich <hello@warren.codes>
2020-10-05 14:22:32 -0600

    Add eventlistener and fetch for snack search form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 // Test that this file is loaded properly!
-alert( 'I\'m here!' );
+// alert( 'I\'m here!' );
 
+/**
+ * Retrieve the form, input, and ul.
+ */
+const snackSearchForm = document.getElementById( 'search-form' );
+const snackSearchInput = document.getElementById( 'search-input' );
+const searchResultsList = document.getElementById( 'search-results' );
 
-
+/**
+ * Listen for a submit.
+ */
+snackSearchForm.addEventListener( 'submit', event => {
+  // Stop the form from submitting the traditional way.
+  event.preventDefault();
+  // Attempt a fetch for results.
+  fetch ( `http://localhost:80/api/snacks.php` )
+    .then( response => response.json() )
+    .then( data => {
+      console.log( data );
+    } )
+} );
