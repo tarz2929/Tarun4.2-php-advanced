597f1eaa40f5ab6b4267491eec64886d303af048
Warren Uhrich <hello@warren.codes>
2020-10-05 10:41:52 -0600

    Add daily cat fact request and var_dump.

--- cat-facts-api.php
+++ cat-facts-api.php
@@ -1,8 +1,12 @@
 <?php
 $GLOBALS['pageTitle'] = 'Cat Facts (External API Test)';
 include './templates/header.php';
-?>
 
+// Retrieve data from API endpoint.
+// @link https://alexwohlbruck.github.io/cat-facts/docs/
+$dailyCatFactResponse = file_get_contents( 
+  'https://cat-fact.herokuapp.com/facts/random'
+);
+var_dump( $dailyCatFactResponse );
 
-
-<?php include './templates/footer.php';
+include './templates/footer.php';
