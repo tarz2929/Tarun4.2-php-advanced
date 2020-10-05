d41cdf41ab6a8af3030559b90469f7975e454ff4
Warren Uhrich <hello@warren.codes>
2020-10-05 11:24:22 -0600

    Add 'Request Animal Facts' form; add test query / response.

--- cat-facts-api.php
+++ cat-facts-api.php
@@ -19,4 +19,22 @@ if ( $dailyCatFactResponse )
   <?php
 }
 
+// Animal Fact Request Form.
+?>
+<h2>Request Animal Facts</h2>
+<form action="#" method="POST">
+  <label for="amount">Enter the Amount of Facts:
+  <input type="number" id="amount" name="amount"></label>
+  <label for="animal-type">Enter the Type of Animal:
+  <input type="text" id="animal-type" name="type">
+  </label>
+  <input type="submit" value="Get Animal Facts!">
+</form>
+<?php
+// Let's modify our request to include a QUERY PARAMETER STRING.
+$factsListResponse = file_get_contents(
+  'https://cat-fact.herokuapp.com/facts/random?amount=10&animal_type=dog'
+); // Test the response via var_dump()
+var_dump( $factsListResponse );
+
 include './templates/footer.php';
