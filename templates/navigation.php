ded405150abbcdba909d2cc0f884e6f9a5aeea45
Warren Uhrich <hello@warren.codes>
2020-10-06 09:58:15 -0600

    Add React example webpage (todo: add React code.)

--- templates/navigation.php
+++ templates/navigation.php
@@ -26,5 +26,10 @@
         Snack Search (AJAX + API)
       </a>
     </li>
+    <li>
+      <a href="snack-search-react.html">
+        Snack Search (React + AJAX + API)
+      </a>
+    </li>
   </ul>
 </nav>
