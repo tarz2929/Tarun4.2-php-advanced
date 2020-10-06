279e92eb05ba10d3c3cd2c2174c4569d040ec4e6
Warren Uhrich <hello@warren.codes>
2020-10-06 10:02:26 -0600

    Get our React Root element in JS.

---
+++ js/react-example.js
@@ -0,0 +1,7 @@
+// Check that this file is loaded!
+// alert( 'React is present and accounted for!' );
+
+// Grab our React "root" element.
+const reactRoot = document.getElementById( 'react-root' );
+
+
